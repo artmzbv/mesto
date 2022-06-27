43473b4ca69840ed4845c9da0162650c46897d51
artmzbv <58895197+artmzbv@users.noreply.github.com>
2022-06-27 08:30:01 +0200

    Add files via upload

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,8 +3,8 @@ import Popup from "./Popup.js"
 export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
-        this._elementImageExpand = this._popupSelector.querySelector('.image-popup__mask-group');
-        this._elementImageExpandText = this._popupSelector.querySelector('.image-popup__text');
+        this._elementImageExpand = this._popup.querySelector('.image-popup__mask-group');
+        this._elementImageExpandText = this._popup.querySelector('.image-popup__text');
     }
     open = (name, link) => {
         super.open();
