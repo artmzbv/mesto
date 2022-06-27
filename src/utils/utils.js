894b4157e7e30f419242fb2b65dfe1914f46fbfa
artmzbv <58895197+artmzbv@users.noreply.github.com>
2022-06-27 07:56:20 +0200

    Add files via upload

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,10 @@
 //Переменные формы попапа//
 export const profileForm = document.querySelector('.popup__form_profile');
 export const imageForm = document.querySelector('.popup__form_image');
+export const avatarForm = document.querySelector('.popup__form_avatar')
 export const editSubmitButton = profileForm.querySelector('.popup__submit-button')
 export const addSubmitButton = imageForm.querySelector('.popup__submit-button')
-const popupAvatar = document.querySelector('.popup-avatar')
-export const avatarSubmitButton = popupAvatar.querySelector('.popup__submit-button')
+export const avatarSubmitButton = avatarForm.querySelector('.popup__submit-button')
 //Аргументы в FormValidator
 export const config = {
     formSelector: '.popup__form',
