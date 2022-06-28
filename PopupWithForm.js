a64f94a62cc5d6a031e857759aebdb9018fd331b
artmzbv <58895197+artmzbv@users.noreply.github.com>
2022-06-28 06:34:31 +0200

    Add files via upload

---
+++ PopupWithForm.js
@@ -0,0 +1,48 @@
+import Popup from './Popup.js'
+export default class PopupWithForm extends Popup {
+    constructor(popupSelector, handleFormSubmit) {
+        super(popupSelector);
+        this._handleFormSubmit = handleFormSubmit;
+        this._popupForm = this._popup.querySelector('.popup__form');
+        this._submitButton = this._popupForm.querySelector('.popup__submit-button')
+        this._inputs = [...this._popupForm.querySelectorAll('.popup__input')];
+    }
+
+    _getInputValues() {
+        const inputValues = {}
+        this._inputs.forEach((input) => {
+            inputValues[input.name] = input.value
+        })
+        return inputValues
+    }
+
+    setEventListeners() {
+        super.setEventListeners();
+        this._popupForm.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+            this._handleFormSubmit(this._getInputValues())
+        });
+    }
+
+    setDeleteButtonSubmit(newSubmit) {
+        this._handleFormSubmit = newSubmit;
+    }
+
+
+    close() {
+        super.close();
+        this._popupForm.reset();
+    }
+      //Задание 10 - создадим
+    loader(isLoading, popuptext){
+        if (isLoading) {
+        this._submitButton.textContent = 'Сохранение...'
+    } else {
+        if (popuptext === 'add') {
+            this._submitButton.textContent = 'Создать'
+        } else {
+            this._submitButton.textContent = 'Сохранить'
+        }
+    }
+    }
+};
